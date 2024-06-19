d90a94ec9618b9f78af7b0e7dd596e8e35951466
Julieta BACHUR <jbachur@CNP.com.ar>
2024-06-19 17:18:02 -0300

    Seccion 1: Quote - DONE

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,17 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
+import { server } from "./mocks/server";
+
+beforeAll(() => {
+  console.log("Iniciando servidor de msw");
+  server.listen();
+});
+
+afterAll(() => {
+  console.log("Apagando servidor de msw");
+  server.close();
+});
+
+afterEach(() => server.resetHandlers());
